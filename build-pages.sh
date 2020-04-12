edec38722e899822fbfedbb2d6a73a4c802bd179
HuiiBuh <nhaderer1@gmail.com>
2020-04-12 20:08:03 +0200

    Updated build

--- build-pages.sh
+++ build-pages.sh
@@ -1,22 +1,15 @@
 git checkout master
-npm install
 ng build --prod --base-href '/ng-bottom-navigation/'
 
-mv dist/example-app/ example-app
-git add example-app/
+git checkout gh-pages  || exit
 
-git checkout gh-pages
+rm *.txt
+rm *.ico
+rm *.js
+rm *.css
+rm *.html
 
-# shellcheck disable=SC2035
-rm *.txt  || echo ''
-rm *.ico  || echo ''
-rm *.js   || echo ''
-rm *.css  || echo ''
-rm *.html || echo ''
-
-mv example-app/* .
-git reset example-app/
-rm -r example-app
+cp -r dist/example-app/* .
 
 git add .
 git commit -m "Updated github pages"
