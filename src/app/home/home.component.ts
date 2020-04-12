620b866eb10c79190e087a89faae119212722dba
HuiiBuh <nhaderer1@gmail.com>
2020-04-12 19:25:48 +0200

    Added documentation and improved the github pages

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, isDevMode} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get devMode(): boolean {
+    return isDevMode();
   }
-
 }
